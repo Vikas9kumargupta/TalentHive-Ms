3d3ffc921d6b9c053741f52a160fc7c08f331b00
Vikas Kumar Gupta <vikasgup074@gmail.com>
2025-04-10 16:14:57 +0530

    Adding Ithers Parts of Application

---
+++ jobms/src/main/java/com/example/jobms/job/external/Company.java
@@ -0,0 +1,37 @@
+package com.example.jobms.job.external;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+
+    private Long id;
+    private String name;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
