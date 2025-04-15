fe748065e79a48cde65d19fc6cb20ec6d07a824e
Vikas Kumar Gupta <vikasgup074@gmail.com>
2025-04-15 13:50:57 +0530

    Implementing Fault Tolerance and Rate Limiting

--- jobms/src/main/java/com/example/jobms/job/impl/JobServiceImpl.java
+++ jobms/src/main/java/com/example/jobms/job/impl/JobServiceImpl.java
@@ -9,6 +9,9 @@ import com.example.jobms.job.dto.JobDTO;
 import com.example.jobms.job.external.Company;
 import com.example.jobms.job.external.Review;
 import com.example.jobms.job.mapper.JobMapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -32,6 +35,8 @@ public class JobServiceImpl implements JobService {
     private CompanyClient companyClient;
     private ReviewClient reviewClient;
 
+    Integer attempt = 0;
+
     public JobServiceImpl(JobRepository jobRepository,
                           CompanyClient companyClient,
                           ReviewClient reviewClient) {
@@ -41,12 +46,26 @@ public class JobServiceImpl implements JobService {
     }
 
     @Override
+//    @CircuitBreaker(name = "companyBreaker",
+//            fallbackMethod = "companyBreakerFallback")
+//    @Retry(name = "companyBreaker",
+//            fallbackMethod = "companyBreakerFallback")
+    @RateLimiter(name = "companyBreaker",
+            fallbackMethod = "companyBreakerFallback")
     public List<JobDTO> findAll() {
+        System.out.println("Attempt:" + ++attempt);
         List<Job> jobs = jobRepository.findAll();
         List<JobDTO> jobDTOS = new ArrayList<>();
         return jobs.stream().map(this::convertToDto)
                 .collect(Collectors.toList());
     }
+
+    public List<String> companyBreakerFallback(Exception e){
+        List<String> list = new ArrayList<>();
+        list.add("Dummy");
+        return list;
+    }
+
     private JobDTO convertToDto(Job job){
         Company company = companyClient.getCompany(job.getCompanyId());
         List<Review> reviews = reviewClient.getReviews(job.getCompanyId());
