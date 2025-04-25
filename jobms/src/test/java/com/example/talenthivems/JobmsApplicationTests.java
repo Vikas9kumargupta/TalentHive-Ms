940c0a529bac828e8ce391129517d9de14f52336
Vikas Kumar Gupta <vikasgup074@gmail.com>
2025-04-25 13:29:25 +0530

    Deploying Kubernetes

--- jobms/src/test/java/com/example/talenthivems/JobmsApplicationTests.java
+++ jobms/src/test/java/com/example/talenthivems/JobmsApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.talenthivems;
 
+import com.example.jobms.JobmsApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = JobmsApplication.class)
 class JobmsApplicationTests {
 
 	@Test
